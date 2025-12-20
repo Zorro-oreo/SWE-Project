eb2b5a8cd7a6c705aac7c57a66e594eb091d0be2
Hazem Refaat <s-hazem.refaat@zewailcity.edu.eg>
2025-12-20 14:20:00 +0200

    Revert "fixed professor repo"

--- srs/repositories/professorRepo.py
+++ srs/repositories/professorRepo.py
@@ -3,11 +3,6 @@ class professorRepo:
 
         self.db = db_conn
         self.pID = professorID
-        
-    def get_courses(self):
-        sql = "SELECT cID, cname FROM Course WHERE PrID = ?"
-        rows = self.db.execute(sql, (self.pID,)).fetchall()
-        return [dict(row) for row in rows]
 
     def get_students_in_course(self, course_id: str):
 
@@ -35,10 +30,20 @@ class professorRepo:
         ''', (grade, student_id, course_id))
         
         self.db.commit()
-        
+        self.db.close()
         
         return f"Grade {grade} has been assigned to student {student_id} for course {course_id}"
     
-    def getLogin(self, id, password):
-
-        return self.db.execute("""SELECT pname FROM Professor WHERE pID = ? AND pass = ?""", (id, password)).fetchone()
+    def getLogin(self, id, password=None):
+        if password:
+            return self.db.execute("""SELECT pID, pname, pass FROM Professor WHERE pID = ? AND pass = ?""", (id, password)).fetchone()
+        else:
+            return self.db.execute("""SELECT pID, pname, pass FROM Professor WHERE pID = ?""", (id,)).fetchone()
+        
+    def get_courses(self):
+        sql = """SELECT cID, cname FROM Course WHERE PrID = ?;"""
+        cursor = self.db.execute(sql, (self.pID,))
+        rows = cursor.fetchall()
+        courses = [{'course_id': row[0], 'course_name': row[1]} for row in rows]
+        return courses
+    
